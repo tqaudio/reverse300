9212101053e948f63fda4c0d55134bc4297afc01
Yoshiyuki Koyanagi <moutend@gmail.com>
2019-04-12 06:53:43 +0900

    Support vst3sdk v3.6.13

--- my_plugins/Reverse300/source/factory.cpp
+++ my_plugins/Reverse300/source/factory.cpp
@@ -1,17 +1,9 @@
-/*
- * Author: Yoshiyuki Koyanagi <moutend@gmail.com>
- * Copyright: Yoshiyuki Koyanagi
- * License: GNU General Public License version 3
- */
-
-#include "public.sdk/source/main/pluginfactoryvst3.h"
+#include "public.sdk/source/main/pluginfactory.h"
 
 #include "../include/controller.h"
 #include "../include/id.h"
 #include "../include/processor.h"
 #include "../include/version.h"
-
-#include "public.sdk/source/main/pluginfactoryvst3.h"
 
 BEGIN_FACTORY_DEF(stringCompanyName, stringCompanyWeb, stringCompanyEmail)
 DEF_CLASS2(INLINE_UID_FROM_FUID(Reverse300::ProcessorID),
